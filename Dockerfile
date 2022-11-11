8513a747decf385ae0dcde3993c319bd8c6a7a67
mindy <mindy.shih@gmail.com>
2022-11-11 19:58:42 +0800

    111101

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,19 @@
-FROM golang:alpine
-#docker中的工作目錄
-WORKDIR $GOPATH/src/gin_docker
+# build stage
+FROM golang:latest AS buildStage
+#docker中的工作目錄 加了AS buildStage 表示僅僅是編譯階段
+WORKDIR $GOPATH/src/firstweb_docker
 # 将当前目录同步到docker工作目录下，也可以只配置需要的目录和文件（配置目录、编译后的程序等）
 ADD . ./
 # 由于所周知的原因，某些包会出现下载超时。这里在docker里也使用go module的代理服务
 ENV GO111MODULE=on
 ENV GOPROXY="https://goproxy.io"
-#指定编译完成后的文件名，可以不设置使用默认的，最后一步要执行该文件名
-RUN go build -o gin_docker .
+#在文件夾將source進行編譯指定编译完成后的文件名，可以不设置使用默认的，最后一步要执行该文件名
+RUN go build -o firstweb_docker .
+
+FROM alpine:latest
+WORKDIR /fweb
+COPY --from=buildStage $GOPATH/src/firstweb_docker /fweb/ 
 EXPOSE 9999
 # 这里跟编译完的文件名一致
-ENTRYPOINT ["./gin_docker"]
+ENTRYPOINT ["./fweb"]
 
--- logrus/logrus.go
+++ logrus/logrus.go
@@ -3,25 +3,60 @@ package logrus
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"log"
 	"os"
+	"path"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/natefinch/lumberjack"
 	"github.com/sirupsen/logrus"
 )
 
-var LogInstance = logrus.New()
+func Logger() *logrus.Logger {
+	//now := time.Now()
+	logFilePath := ""
+	if dir, err := os.Getwd(); err == nil {
+		logFilePath = dir + "/logs/"
+	}
+	if err := os.MkdirAll(logFilePath, 0777); err != nil {
+		fmt.Println(err.Error())
+	}
+	logFileName := "log.log"
+	//日誌檔案
+	fileName := path.Join(logFilePath, logFileName)
+	if _, err := os.Stat(fileName); err != nil {
+		if _, err := os.Create(fileName); err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+	//寫入檔案
+	src, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
+	//例項化
+	logger := logrus.New()
+
+	//設定輸出
+	logger.Out = src
+
+	//設定日誌級別
+	logger.SetLevel(logrus.DebugLevel)
+
+	//設定日誌格式
+	logger.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+	return logger
+}
 
 func Logrus() gin.HandlerFunc {
-	// 設置日誌記錄級別//設置日誌級別为TraceLevel，为了能看到Trace和Debug日志
-	logrus.SetLevel(logrus.TraceLevel)
-
+	logger := Logger()
 	return func(c *gin.Context) {
 		path := c.Request.URL.Path
+
+		// Complete log
 		body, _ := ioutil.ReadAll(c.Request.Body)
 		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 		c.Next()
@@ -30,32 +65,21 @@ func Logrus() gin.HandlerFunc {
 		for k, v := range c.Request.Header {
 			header += k + ": " + fmt.Sprint(v) + "\r\n"
 		}
-		//寫進文件
-		writer1 := &bytes.Buffer{}
-		writer2 := os.Stdout
-		writer3, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE, 6379)
-		if err != nil {
-			log.Fatalf("create file log.txt failed: %v", err)
-		}
-
-		LogInstance.SetOutput(&lumberjack.Logger{
-
-			MaxSize:    10, // 單文件最大容量,單位是MB
-			MaxBackups: 30, // 最大保留過期文件個數
-			MaxAge:     1,  // 保留過期文件的最大時間間隔,單位是天
-
-		})
-		// 同時將日誌寫入文件和控制檯
-		logrus.SetOutput(io.MultiWriter(writer1, writer2, writer3))
 
 		if c.Request.URL.RawQuery != "" {
 			path += "?" + c.Request.URL.RawQuery
 		}
+		// 開始時間
+		startTime := time.Now()
+		// 結束時間
+		endTime := time.Now()
+		// 執行時間
+		latencyTime := endTime.Sub(startTime)
 
 		errorMsg := c.Keys["ErrorMsg"]
 
-		logrus.WithField("HTTPResponse", LogResponse{
-			Time:   time.Now().Format("2006/01/02 15:04:05"),
+		logger.WithField("HTTPResponse", LogResponse{
+			Time:   latencyTime.String(),
 			IP:     c.ClientIP(),
 			Method: c.Request.Method,
 			Path:   path,
@@ -79,110 +103,3 @@ type LogResponse struct {
 	Params string
 	Error  interface{} `json:"FailMsg,omitempty"`
 }
-
-// 	logrus.Info("just some info logging...")
-// 	//設置日誌級別为TraceLevel，为了能看到Trace和Debug日志
-// 	logrus.SetLevel(logrus.TraceLevel)
-// 	// 設置日誌格式爲json格式
-// 	//log.SetFormatter(&log.JSONFormatter{})
-
-// 	logrus.Trace("trace msg")
-// 	logrus.Debug("debug msg")
-// 	logrus.Info("info msg")
-// 	//logrus.Warn("warn msg")
-// 	logrus.Error("error msg")
-// 	//logrus.Fatal("fatal msg")
-// 	//logrus.Panic("panic msg")
-
-// 	//time：输出日志的时间
-// 	//level：日志级别
-// 	//msg：日志信息。
-
-// 	//调用logrus.SetReportCaller(true)设置在输出日志中添加文件名和方法信息
-// 	logrus.SetReportCaller(true)
-
-// 	//有时候需要在输出中添加一些字段，可以通过调用logrus.WithField和logrus.WithFields实现。
-// 	//logrus.WithFields接受一个logrus.Fields类型的参数，其底层实际上为map[string]interface{}：
-
-// 	writer1 := &bytes.Buffer{}
-// 	writer2 := os.Stdout
-// 	writer3, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE, 6379)
-// 	if err != nil {
-// 		log.Fatalf("create file log.txt failed: %v", err)
-// 	}
-// 	//默认情况下，日志输出到io.Stderr。可以调用logrus.SetOutput传入一个io.Writer参数。
-// 	logrus.SetOutput(io.MultiWriter(writer1, writer2, writer3))
-// 	//传入一个io.MultiWriter， 同时将日志写到bytes.Buffer、标准输出和文件中
-
-// 	logrus.Info("info msg")
-// }
-
-// }
-// //type Hook interface {
-// //Levels()方法返回感兴趣的日志级别，输出其他日志时不会触发钩子
-// //Levels() []Level
-// //Fire是日志输出前调用的钩子方法。
-// //Fire(*Entry) error
-// //}
-
-// func init() {
-// 	hookConfig := logredis.HookConfig{
-// 		Host:     "localhost",
-// 		Key:      "abc",
-// 		Format:   "v1",
-// 		App:      "firstweb",
-// 		Port:     6379,
-// 		Hostname: "my_app_hostname",
-// 		//DB:       0, // optional
-// 		TTL: 3600,
-// 	}
-
-// 	hook, err := logredis.NewHook(hookConfig)
-// 	if err == nil {
-// 		logrus.AddHook(hook)
-// 	} else {
-// 		logrus.Errorf("logredis error: %q", err)
-// 	}
-// }
-
-// func Logrus() {
-
-// 	//設置日誌級別为TraceLevel，为了能看到Trace和Debug日志
-// 	logrus.SetLevel(logrus.TraceLevel)
-// 	// 設置日誌格式爲json格式
-// 	//log.SetFormatter(&log.JSONFormatter{})
-
-// 	logrus.Trace("trace msg")
-// 	//logrus.Debug("debug msg")
-// 	logrus.Info("info msg")
-// 	//logrus.Warn("warn msg")
-// 	logrus.Error("error msg")
-// 	//logrus.Fatal("fatal msg")
-// 	//logrus.Panic("panic msg")
-
-// 	//logrus.Panic("panic msg")
-// 	//time：输出日志的时间
-// 	//level：日志级别
-// 	//msg：日志信息。
-
-// 	//调用logrus.SetReportCaller(true)设置在输出日志中添加文件名和方法信息
-// 	logrus.SetReportCaller(true)
-
-// 	//有时候需要在输出中添加一些字段，可以通过调用logrus.WithField和logrus.WithFields实现。
-// 	//logrus.WithFields接受一个logrus.Fields类型的参数，其底层实际上为map[string]interface{}：
-
-// 	writer1 := &bytes.Buffer{}
-// 	writer2 := os.Stdout
-// 	writer3, err := os.OpenFile("/logrus"+".log.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
-// 	//os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE, 6379)
-// 	//logfile, _ :=
-// 	if err != nil {
-// 		log.Fatalf("create file log.txt failed: %v", err)
-// 	}
-// 	//默认情况下，日志输出到io.Stderr。可以调用logrus.SetOutput传入一个io.Writer参数。
-// 	logrus.SetOutput(io.MultiWriter(writer1, writer2, writer3))
-// 	//传入一个io.MultiWriter， 同时将日志写到bytes.Buffer、标准输出和文件中
-
-// 	logrus.Info("info msg")
-
-// }
